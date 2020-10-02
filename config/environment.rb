602eabde1bdcd2b19bd76af0fc647d0f1cb566fd
daniyellalove <68965224+daniyellalove@users.noreply.github.com>
2020-10-02 02:19:01 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
+require_relative '../lib/konguage/scaper'
+require_relative '../lib/konguage/plan
+require_relative '../lib/konguage/cli
+require_relative '../lib/konguage/version
